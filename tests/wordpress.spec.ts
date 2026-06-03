a6b48c3bd37c33986d636d2c341a2879d7390923
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-06-02 22:13:04 -0300

    added - testes de login e post de comentário.

---
+++ tests/wordpress.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("E2E Wordpress Playground", () => {
+    test.beforeEach(async ({ page }) => {
+
+        await page.goto("https://sosfinance.joaoc.dev", {
+            timeout: 60000,
+        })
+    });
+
+    test("teste", async ({ page }) => {
+
+
+        const menuButton = page.getByRole("link", { name: "Educação Financeira" });
+
+        await menuButton.click();
+
+        const articleLink = page.locator('h2').getByRole('link', { name: "Por que é importante guardar" });
+
+        await articleLink.click();
+
+        const loginLink = page.getByRole('link',{name : 'login'}).click();
+
+        await page.fill("#user_login", "etec");
+        await page.fill("#user_pass", "etec123@@");
+        await page.click("#wp-submit");
+
+        await page.fill("#comment","papai cris");
+        await page.click("#submit");
+
+       // page.waitForTimeout(5000);
+        });
+    });
